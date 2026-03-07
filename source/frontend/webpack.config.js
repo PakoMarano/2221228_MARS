503eb0ed34de211fddbd885bd3372b39af43fb52
Roxana <roxana.peto2358@gmail.com>
2026-03-08 00:46:06 +0100

    Fix icon

--- source/frontend/webpack.config.js
+++ source/frontend/webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
     },
     devtool: 'cheap-module-source-map',
     devServer: {
-        static: './dist',
+        static: [
+            path.join(__dirname, 'dist'),
+            path.join(__dirname, 'public')
+        ],
         port: 3000,
         hot: true,
         open: true,
